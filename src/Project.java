d6773b2d258efa0d5d9fe5547c6ef049f7925d10
Kambiz29 <kgunter@bluewin.ch>
2022-02-24 20:18:43 +0100

    Main w/ input

--- src/Project.java
+++ src/Project.java
@@ -1,5 +1,25 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Victor Garvalov
  */
 public class Project {
+    String nameProj ;
+    int nDays  ;
+    int projScore  ;
+    int projDeadline  ;
+    int nRoles ;
+    List<Skill> skills ;
+
+    public Project(String nameProj, int nDays, int projScore, int projDeadline, int nRoles, List<Skill> skills) {
+        this.nameProj = nameProj;
+        this.nDays = nDays;
+        this.projScore = projScore;
+        this.projDeadline = projDeadline;
+        this.nRoles = nRoles;
+        this.skills = skills;
+    }
 }
