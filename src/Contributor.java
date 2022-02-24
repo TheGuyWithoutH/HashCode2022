5e292f5d2c4c6893cb1f4b2eeb6d7ea591c08a32
TheGuyWithoutH <ugo.balducci@epfl.ch>
2022-02-24 19:47:41 +0100

    Ugo #1

--- src/Contributor.java
+++ src/Contributor.java
@@ -1,5 +1,44 @@
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Victor Garvalov
  */
 public class Contributor {
+
+    public String name;
+    public Map<String, Integer> skills;
+    private boolean taken;
+
+
+    public Contributor(String name, Map<String, Integer> skills) {
+        this.name = name;
+        this.skills = skills;
+        taken = false;
+    }
+
+    public Map<String, Integer> getSkills() {
+        return skills;
+    }
+
+    public void incrementSkill(String name) {
+        skills.get(name);
+    }
+
+    public void hire(){
+        taken=true;
+    }
+
+    public void release(){
+        taken = false;
+    }
+
+    public boolean available() {
+        return taken;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
