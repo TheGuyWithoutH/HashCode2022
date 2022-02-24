097c823d31e55b11ce2d3664bce07e5424ab0955
TheGuyWithoutH <ugo.balducci@epfl.ch>
2022-02-24 21:32:54 +0100

    Ugo #7 Solve

--- src/Solve.java
+++ src/Solve.java
@@ -5,18 +5,21 @@ public class Solve {
 
     public static void solve(List<Contributor> contributors, List<Project> projects, List<String> skills){
         List<Project> toDo = new ArrayList<>(projects);
+        int i = 0;
 
         while(!toDo.isEmpty()) {
 
             List<Contributor> available = contributors.stream().filter(Contributor::available).toList();
             List<Integer> skillWanted = Skill.skillsScore(skills, projects, available);
 
-            for (Project project : projects) {
+            for (Project project : toDo) {
                 if(project.isFinished()) {
-                    projects.remove(project);
+                    toDo.remove(project);
                 }
                 project.update();
             }
+
+            i++;
 
         }
 
